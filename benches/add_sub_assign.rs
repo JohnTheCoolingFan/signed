6c9db19a5aae6cbb9ff02daf6e7d3632f105c82d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-16 20:09:54 +0300

    Added other number types

--- benches/add_sub_assign.rs
+++ benches/add_sub_assign.rs
@@ -8,7 +8,43 @@ fn add_assign<I: Signed>(mut n: I, other: I) -> I {
     n
 }
 
-fn benchmark(c: &mut Criterion) {
+fn sub_assign<I: Signed>(mut n: I, other: I) -> I {
+    let mut abs = n.get_absolute();
+    abs -= &other;
+    n
+}
+
+fn benchmark_add_i8(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("add_assign_i8", |b| {
+        b.iter_batched(
+            || {
+                let n: i8 = rng.gen();
+                let other: i8 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| add_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_add_i16(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("add_assign_i16", |b| {
+        b.iter_batched(
+            || {
+                let n: i16 = rng.gen();
+                let other: i16 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| add_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_add_i32(c: &mut Criterion) {
     let mut rng = thread_rng();
     c.bench_function("add_assign_i32", |b| {
         b.iter_batched(
@@ -23,5 +59,125 @@ fn benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark);
-criterion_main!(benches);
+fn benchmark_add_i64(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("add_assign_i64", |b| {
+        b.iter_batched(
+            || {
+                let n: i64 = rng.gen();
+                let other: i64 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| add_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_add_i128(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("add_assign_i128", |b| {
+        b.iter_batched(
+            || {
+                let n: i128 = rng.gen();
+                let other: i128 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| add_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_sub_i8(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("sub_assign_i8", |b| {
+        b.iter_batched(
+            || {
+                let n: i8 = rng.gen();
+                let other: i8 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| sub_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_sub_i16(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("sub_assign_i16", |b| {
+        b.iter_batched(
+            || {
+                let n: i16 = rng.gen();
+                let other: i16 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| sub_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_sub_i32(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("sub_assign_i32", |b| {
+        b.iter_batched(
+            || {
+                let n: i32 = rng.gen();
+                let other: i32 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| sub_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_sub_i64(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("sub_assign_i64", |b| {
+        b.iter_batched(
+            || {
+                let n: i64 = rng.gen();
+                let other: i64 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| sub_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+fn benchmark_sub_i128(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    c.bench_function("sub_assign_i128", |b| {
+        b.iter_batched(
+            || {
+                let n: i128 = rng.gen();
+                let other: i128 = rng.gen();
+                (n, other)
+            },
+            |(n, other)| sub_assign(black_box(n), black_box(other)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(
+    benches_add,
+    benchmark_add_i8,
+    benchmark_add_i16,
+    benchmark_add_i32,
+    benchmark_add_i64,
+    benchmark_add_i128
+);
+criterion_group!(
+    benches_sub,
+    benchmark_sub_i8,
+    benchmark_sub_i16,
+    benchmark_sub_i32,
+    benchmark_sub_i64,
+    benchmark_sub_i128
+);
+criterion_main!(benches_add, benches_sub);
--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,93 @@ pub struct Absolute<'a, I: Signed> {
     num: &'a mut I,
 }
 
+impl Signed for i8 {
+    type Unsigned = u8;
+    const ZERO: i8 = 0;
+
+    fn get_absolute(&mut self) -> Absolute<'_, Self> {
+        Absolute { num: self }
+    }
+
+    fn abs_diff(self, other: Self) -> Self::Unsigned {
+        self.abs_diff(other)
+    }
+
+    fn is_positive(self) -> bool {
+        self.is_positive()
+    }
+
+    fn is_negative(self) -> bool {
+        self.is_negative()
+    }
+}
+
+impl Signed for i16 {
+    type Unsigned = u16;
+    const ZERO: i16 = 0;
+
+    fn get_absolute(&mut self) -> Absolute<'_, Self> {
+        Absolute { num: self }
+    }
+
+    fn abs_diff(self, other: Self) -> Self::Unsigned {
+        self.abs_diff(other)
+    }
+
+    fn is_positive(self) -> bool {
+        self.is_positive()
+    }
+
+    fn is_negative(self) -> bool {
+        self.is_negative()
+    }
+}
+
 impl Signed for i32 {
     type Unsigned = u32;
     const ZERO: i32 = 0;
+
+    fn get_absolute(&mut self) -> Absolute<'_, Self> {
+        Absolute { num: self }
+    }
+
+    fn abs_diff(self, other: Self) -> Self::Unsigned {
+        self.abs_diff(other)
+    }
+
+    fn is_positive(self) -> bool {
+        self.is_positive()
+    }
+
+    fn is_negative(self) -> bool {
+        self.is_negative()
+    }
+}
+
+impl Signed for i64 {
+    type Unsigned = u64;
+    const ZERO: i64 = 0;
+
+    fn get_absolute(&mut self) -> Absolute<'_, Self> {
+        Absolute { num: self }
+    }
+
+    fn abs_diff(self, other: Self) -> Self::Unsigned {
+        self.abs_diff(other)
+    }
+
+    fn is_positive(self) -> bool {
+        self.is_positive()
+    }
+
+    fn is_negative(self) -> bool {
+        self.is_negative()
+    }
+}
+
+impl Signed for i128 {
+    type Unsigned = u128;
+    const ZERO: i128 = 0;
 
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
