f107ff48fd8d4b895f5ba82bcf40434af62ac787
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-02-16 20:23:00 +0300

    Improved perfoemance plus coupel of new apis

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,13 @@ where
 
     fn abs_diff(self, other: Self) -> Self::Unsigned;
 
+    fn abs(self) -> Self;
+
     fn is_positive(self) -> bool;
 
     fn is_negative(self) -> bool;
+
+    fn signum(self) -> Self;
 }
 
 pub struct Absolute<'a, I: Signed> {
@@ -24,20 +28,34 @@ impl Signed for i8 {
     type Unsigned = u8;
     const ZERO: i8 = 0;
 
+    #[inline]
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
     }
 
+    #[inline]
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    #[inline]
     fn abs_diff(self, other: Self) -> Self::Unsigned {
         self.abs_diff(other)
     }
 
+    #[inline]
     fn is_positive(self) -> bool {
         self.is_positive()
     }
 
+    #[inline]
     fn is_negative(self) -> bool {
         self.is_negative()
+    }
+
+    #[inline]
+    fn signum(self) -> Self {
+        self.signum()
     }
 }
 
@@ -45,20 +63,34 @@ impl Signed for i16 {
     type Unsigned = u16;
     const ZERO: i16 = 0;
 
+    #[inline]
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
     }
 
+    #[inline]
     fn abs_diff(self, other: Self) -> Self::Unsigned {
         self.abs_diff(other)
     }
 
+    #[inline]
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    #[inline]
     fn is_positive(self) -> bool {
         self.is_positive()
     }
 
+    #[inline]
     fn is_negative(self) -> bool {
         self.is_negative()
+    }
+
+    #[inline]
+    fn signum(self) -> Self {
+        self.signum()
     }
 }
 
@@ -66,20 +98,34 @@ impl Signed for i32 {
     type Unsigned = u32;
     const ZERO: i32 = 0;
 
+    #[inline]
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
     }
 
+    #[inline]
     fn abs_diff(self, other: Self) -> Self::Unsigned {
         self.abs_diff(other)
     }
 
+    #[inline]
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    #[inline]
     fn is_positive(self) -> bool {
         self.is_positive()
     }
 
+    #[inline]
     fn is_negative(self) -> bool {
         self.is_negative()
+    }
+
+    #[inline]
+    fn signum(self) -> Self {
+        self.signum()
     }
 }
 
@@ -87,20 +133,33 @@ impl Signed for i64 {
     type Unsigned = u64;
     const ZERO: i64 = 0;
 
+    #[inline]
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
     }
 
+    #[inline]
     fn abs_diff(self, other: Self) -> Self::Unsigned {
         self.abs_diff(other)
     }
 
+    #[inline]
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    #[inline]
     fn is_positive(self) -> bool {
         self.is_positive()
     }
 
+    #[inline]
     fn is_negative(self) -> bool {
         self.is_negative()
+    }
+
+    fn signum(self) -> Self {
+        self.signum()
     }
 }
 
@@ -108,20 +167,34 @@ impl Signed for i128 {
     type Unsigned = u128;
     const ZERO: i128 = 0;
 
+    #[inline]
     fn get_absolute(&mut self) -> Absolute<'_, Self> {
         Absolute { num: self }
     }
 
+    #[inline]
     fn abs_diff(self, other: Self) -> Self::Unsigned {
         self.abs_diff(other)
     }
 
+    #[inline]
+    fn abs(self) -> Self {
+        self.abs()
+    }
+
+    #[inline]
     fn is_positive(self) -> bool {
         self.is_positive()
     }
 
+    #[inline]
     fn is_negative(self) -> bool {
         self.is_negative()
+    }
+
+    #[inline]
+    fn signum(self) -> Self {
+        self.signum()
     }
 }
 
